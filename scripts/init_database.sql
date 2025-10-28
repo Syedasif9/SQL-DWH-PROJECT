53766e3b73128c1a7cd61321bca4b431722d8069
Syedasif9 <asifs7110@gmail.com>
2025-10-28 20:12:31 +0530

    Create init_database.sql

---
+++ scripts/init_database.sql
@@ -0,0 +1,15 @@
+/*
+=============================================================
+Create Database and Schemas
+=============================================================
+Script Purpose:
+    This script creates a new database named 'DWH' 
+    Additionally, the script sets up three schemas 
+    within the database: 'bronze', 'silver', and 'gold'.
+*/
+
+create database dwh;
+
+create schema bronze;
+create schema silver;
+create schema gold;
